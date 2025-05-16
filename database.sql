66831d478e06b43445f6f6de46fe898aa0237997
Amirparsatec <amirparsa138585@yahoo.com>
2025-05-16 12:55:51 +0330

    database_p2

--- database.sql
+++ database.sql
@@ -23,3 +23,25 @@ CREATE TABLE melkradar (
     INDEX idx_area (area),
     INDEX idx_location (location(255))
 );
+
+CREATE TABLE divar (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    area INT,
+    built_year INT,
+    room INT,
+    floor INT,
+    max_floor INT,
+    sale BOOLEAN,
+    rent BOOLEAN,
+    elevator BOOLEAN,
+    parking BOOLEAN,
+    storage_room BOOLEAN,
+    image_link TEXT,
+    location TEXT,
+    rent_price BIGINT,
+    deposit_price BIGINT,
+    UNIQUE KEY unique_property (title, area, location(255)),
+    INDEX idx_area (area),
+    INDEX idx_location (location(255))
+);
